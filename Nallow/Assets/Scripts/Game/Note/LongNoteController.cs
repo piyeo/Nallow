48b528660e2c8e7a426669307806de2fdd09eabc
piyeo <sakana.mo17@gmail.com>
2021-01-03 20:51:23 +0900

    ロングノーツの実装

---
+++ Nallow/Assets/Scripts/Game/Note/LongNoteController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Player;
+
+namespace Note
+{
+    public class LongNoteController : NoteControllerBase
+    {
+        #pragma warning disable 0649
+        [SerializeField] GameObject objBegin;
+        [SerializeField] GameObject objEnd;
+        [SerializeField] GameObject objTrail;
+
+        void Update()
+        {
+            Vector2 positionBegin = new Vector2();
+            if (noteProperty.lane == 0) { positionBegin.x = -1.3f; }
+            if (noteProperty.lane == 1) { positionBegin.x = 1.3f; }
+            positionBegin.y = (noteProperty.beatBegin - PlayerController.CurrentBeat) * PlayerController.ScrollSpeed;
+            objBegin.transform.localPosition = positionBegin;
+
+            Vector2 positionEnd = new Vector2();
+            if (noteProperty.lane == 0) { positionEnd.x = -1.3f; }
+            if (noteProperty.lane == 1) { positionEnd.x = 1.3f; }
+            positionEnd.y = (noteProperty.beatEnd - PlayerController.CurrentBeat) * PlayerController.ScrollSpeed;
+            objEnd.transform.localPosition = positionEnd;
+
+            Vector2 positionTrail = (positionBegin + positionEnd) / 2f;
+            objTrail.transform.localPosition = positionTrail;
+
+            Vector2 scale = objTrail.transform.localScale;
+            scale.y = positionEnd.y - positionBegin.y;
+            objTrail.transform.localScale = scale;
+        }
+    }
+}
--- Nallow/Assets/Scripts/Game/Note/SingleNoteController.cs
+++ Nallow/Assets/Scripts/Game/Note/SingleNoteController.cs
@@ -11,8 +11,7 @@ namespace Note
             //1.3f : JudgeZoneのx座標
             if (noteProperty.lane == 0) { position.x = -1.3f; }
             if (noteProperty.lane == 1) { position.x = 1.3f; }
-            position.y = (noteProperty.beatBegin - PlayerController.CurrentBeat) *
-                PlayerController.ScrollSpeed;
+            position.y = (noteProperty.beatBegin - PlayerController.CurrentBeat) * PlayerController.ScrollSpeed;
             transform.localPosition = position;
         }
     }

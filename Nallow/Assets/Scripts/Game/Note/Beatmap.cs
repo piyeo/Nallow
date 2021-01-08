06c7c5a7a2e0da472f19c6dd8f8b9e3a80b16d48
piyeo <sakana.mo17@gmail.com>
2021-01-09 00:43:23 +0900

    譜面(.susファイル)読み込み機能の実装

--- Nallow/Assets/Scripts/Game/Note/Beatmap.cs
+++ Nallow/Assets/Scripts/Game/Note/Beatmap.cs
@@ -7,66 +7,26 @@ namespace Note
     public class Beatmap
     {
         public List<NoteProperty> noteProperties = new List<NoteProperty>();
-        public float tempo { get; set; }
-        //private List<TempoChange> TempoChanges = new List<TempoChange>();
+        public float tempo { get; }
 
-        public static float ToSec(float beat, float tempo)
+        //susファイルを読み込む
+        public Beatmap(string filepath)
         {
-            // 60f : 一分間の秒数
-            return beat / (tempo / 60f);
+            var beatmapLoader = new BeatmapLoader(filepath);
+            noteProperties = beatmapLoader.noteProperties;
+            tempo = beatmapLoader.tempo;
         }
 
-        public static float ToBeat(float sec, float tempo)
+        public static float ToSec(float beat, float _tempo)
         {
             // 60f : 一分間の秒数
-            return sec * (tempo / 60f);
+            return beat / (_tempo / 60f);
         }
 
-        //public static float ToTotalSec(float beat, List<TempoChange> tempoChanges)
-        //{
-        //    float totalSec = 0f;
-        //    // i: テンポ変化番号
-        //    int i = 0;
-        //    // n: 変換するbeatの直前までのテンポ変化の回数
-        //    var n = tempoChanges.Count(x => (x.beat <= beat));
-        //    // 変換するbeatの直前にあるテンポ変化までのsecを求める
-        //    while (i < n - 1)
-        //    {
-        //        totalSec += ToSec(tempoChanges[i+1].beat - tempoChanges[i].beat, tempoChanges[i].tempo);
-        //        i++;
-        //    }
-        //    // 残りのbeat分を足す
-        //    totalSec += ToSec(beat - tempoChanges[i].beat, tempoChanges[i].tempo);
-        //    return totalSec;
-        //}
-
-        //// テンポ変化情報を基に、secをbeatへ変換する
-        //public static float ToTotalBeat(float sec, List<TempoChange> tempoChanges)
-        //{
-        //    float totalSec = 0;
-        //    // i: テンポ変化番号
-        //    int i = 0;
-        //    // n: 全てのテンポ変化の回数
-        //    var n = tempoChanges.Count;
-        //    // 最後から1つ前のテンポ変化までループ
-        //    while (i < n - 1)
-        //    {
-        //        // tmpSec: i回目のテンポ変化地点での秒数
-        //        var tmpSec = totalSec;
-        //        // 次(i+1回目)のテンポ変化タイミング(秒)を計算する
-        //        totalSec += ToSec(tempoChanges[i+1].beat - tempoChanges[i].beat, tempoChanges[i].tempo);
-        //        if (totalSec >= sec)
-        //        {
-        //            // 次のテンポ変化タイミングが変換するsecを超えた場合、
-        //            //「超える直前のテンポ変化があるbeat + 残りのbeat」を返す
-        //            return tempoChanges[i].beat + ToBeat(sec - tmpSec, tempoChanges[i].tempo);
-        //        }
-        //        i++;
-        //    }
-        //    // 変換するsecが最後のテンポ変化よりも後にある時、
-        //    //「最後のテンポ変化があるbeat + 残りのbeat」を返す
-        //    return tempoChanges[n - 1].beat + ToBeat(sec - totalSec, tempoChanges[n - 1].tempo
-        //    );
-        //}
+        public static float ToBeat(float sec, float _tempo)
+        {
+            // 60f : 一分間の秒数
+            return sec * (_tempo / 60f);
+        }
     }
 }
--- Nallow/Assets/Scripts/Game/Player/PlayerController.cs
+++ Nallow/Assets/Scripts/Game/Player/PlayerController.cs
@@ -18,25 +18,15 @@ namespace Player
         public static float CurrentBeat = 0f;
 
         public static Beatmap beatmap { get; set; }
-        private static readonly float startOffset = 1.0f;
+        private static readonly float startOffset = 5.0f;
 
         private void Awake()
         {
             CurrentSec = 0f;
             CurrentBeat = 0f;
 
-            beatmap = new Beatmap();
-
-            beatmap.noteProperties = new List<NoteProperty>
-            {
-                new NoteProperty(1, 1, 0, NoteType.Single),
-                new NoteProperty(2, 2, 1, NoteType.Single),
-                new NoteProperty(4, 4, 0, NoteType.Single),
-                new NoteProperty(6, 9, 0, NoteType.Long),
-                new NoteProperty(11, 11, 1, NoteType.Flick)
-            };
-
-            beatmap.tempo = 60f;
+            var beatmapDirectory = Application.dataPath + "/../Beatmaps";
+            beatmap = new Beatmap(beatmapDirectory + "/Barduckman_NORMAL.sus");
 
             foreach (var _noteProperty in beatmap.noteProperties)
             {
